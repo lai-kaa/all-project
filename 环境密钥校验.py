97c0f93a890c11d47b20e0653a21545d22a537da
lai-kaa <3579531196@qq.com>
2026-07-13 19:59:17 +0800

    Delete 环境密钥校验.py

--- 环境密钥校验.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os, sys
-load_dotenv()
-required = ["DEEPSEEK_API_KEY","DEEPSEEK_BASE_URL"]
-missing = [k for k in required if not os.getenv(k)]
-if missing:
- print(",".join(missing))
- sys.exit(1)
