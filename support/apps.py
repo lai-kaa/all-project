feafeac3d3cde6d1e09ac734de28a407e66c785c
lai-kaa <3579531196@qq.com>
2026-07-13 20:38:07 +0800

    Delete support directory

--- support/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SupportConfig(AppConfig):
-    """智能客服：疑难解答 + DeepSeek V4"""
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'support'
-    verbose_name = '智能客服'
