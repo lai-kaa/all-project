5f76d23571b52891f5967af1a80a06fd81163bb4
lai-kaa <3579531196@qq.com>
2026-07-13 20:38:20 +0800

    Delete ticket directory

--- ticket/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TicketConfig(AppConfig):
-    """车次与票务应用"""
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ticket'
-    verbose_name = '铁路票务'
--- ticket/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# 票务相关路由：查询页 / 购票
-urlpatterns = [
-    path('', views.ticket, name='ticket'),
-    path('buy/', views.buy_ticket, name='buy_ticket'),
-]
